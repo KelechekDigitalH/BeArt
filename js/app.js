e92e3e05aad531f03b8be371eddfb49731f6ba7e
Tukashov <erbol.tukashov.kk@gmail.com>
2022-06-02 23:45:04 +0600

    feat: add animate svg circle line

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const ourCommitment = new Vivus(
+  'line-commitment',
+  {
+    type: 'sync',
+    duration: 150,
+  },
+);
+
+const ourCommunity = new Vivus(
+  'line-community',
+  {
+    type: 'sync',
+    duration: 150,
+  },
+);
+
+const roadmap = new Vivus(
+  'line-roadmap',
+  {
+    type: 'sync',
+    duration: 150,
+  },
+);
+
+const ourTeam = new Vivus(
+  'line-our-team',
+  {
+    type: 'sync',
+    duration: 150,
+  },
+);
+
+const ourPartners = new Vivus(
+  'line-our-partners',
+  {
+    type: 'sync',
+    duration: 150,
+  },
+);
