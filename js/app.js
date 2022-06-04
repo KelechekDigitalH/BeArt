927093f1e9c06224786c41c6883828bb4ab406b7
Myrzaiym Askarova <myrzaiym.askarova.kk@gmail.com>
2022-06-05 03:53:55 +0600

    add slider

--- js/app.js
+++ js/app.js
@@ -45,3 +45,32 @@ const footer = new Vivus(
     duration: 150,
   },
 );
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 15,
+  centeredSlides: true,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
+
+const infoTitle = document.querySelector(".info-name");
+const infoInfo = document.querySelector(".info-info");
+const swiperActive = document.querySelector("swiper-slide-active");
+const swiperSlide = document.querySelector(".swiper-slide");
+
+
+
+if(swiperSlide.classList.contains('active')){
+ 
+ infoTitle.classList.toggle();
+ 
+}
